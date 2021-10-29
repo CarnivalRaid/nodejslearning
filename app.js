d88ab8925f9797fc9bbb818ab0d4cd74f35692a6
carnivalraid <game.aspirina@gmail.com>
2021-10-29 20:27:31 +0300

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-const {readFile, writeFile} = require('fs')
+const os = require('os')
+const path = require('path')
+const myPC = {
+    osType: os.type(),
+    osName: os.release(),
+    freeMem: os.freemem()/1024/1024/1024,
+    totalMem: os.totalmem(),
+}
 
-readFile('1-intro.js', 'utf8', (err, result) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    const first = result
-    readFile('nonexistant.txt', 'utf8', (err, result) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                writeFile('nonexistant.txt', first, (err, result) => {
-                    if (err) {
-                    console.log(err) 
-                    return}
-                    console.log(first)
-                })
-            }
-            }
-        console.log(result)
-    })
+console.log(myPC)
 
+const {readFile, writeFile, access} = require('fs')
+const { F_OK } = require('constants')
+
+const myFile = path.basename('./content/first.text')
+console.log(myFile)
+const readCliText = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
+
+access(myFile, F_OK, (err)=> {
+    if (err) {
+        console.log('Some error')
+        console.log('Please introduce the correct fileName: ')
+        const newFile = readCliText.question(`Please introduce the file name: `, name => {
+            console.log(`The file name is ${name}`)
+            readCliText.close()
+            return name
+
+        })
+    }})
